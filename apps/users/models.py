45d4c7c491be5dbbdf59c0c394bb81ff7e8d8c20
danielhuamani <danielhuamani15@gmail.com>
2021-02-12 20:57:58 -0500

    add structure and removed pyc #1

---
+++ apps/users/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from .managers import UserManager
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(_('first name'), max_length=120, blank=True)
+    last_name = models.CharField(_('last name'), max_length=120, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    type_user = models.CharField(_("type user"), choices=CHOICES_TYPE_USER, max_length=50)
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
