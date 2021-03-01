580966b16ef0a9e97f476a99c1ab2046911c2ba2
danielhuamani <danielhuamani15@gmail.com>
2021-02-28 20:39:32 -0500

    fix apis token

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (
 app_name = 'users'
 
 urlpatterns = [
-    path('api/token/', UserLoginAPI.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('token/', UserLoginAPI.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('user/register', UserRegisterAPI.as_view()),
     path('user/me/', UserProfileAPI.as_view(), name='user_profile'),
     path('user/change-password/', UserChangePasswordAPI.as_view(), name='user_change_password'),
