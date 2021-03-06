2507aab84606bfc52be29de8b7fbf9a8d2f1a0b3
danielhuamani <danielhuamani15@gmail.com>
2021-03-05 21:49:03 -0500

    add serializers

--- apps/core/permissions.py
+++ apps/core/permissions.py
@@ -1,8 +1,8 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import SessionAuthentication
-from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class AuthenticatedJWT:
-    authentication_classes = [SessionAuthentication, JWTTokenUserAuthentication]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
