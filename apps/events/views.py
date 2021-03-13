56b54d00e7afce33cf807c095ebbfbcba20b2a46
danielhuamani <danielhuamani15@gmail.com>
2021-03-13 12:06:12 -0500

    add user envet

--- apps/events/views.py
+++ apps/events/views.py
@@ -7,10 +7,20 @@ from rest_framework.generics import ListAPIView
 from apps.core.permissions import AuthenticatedJWT
 from .serializers import EventSerializer, EventRegisterSerializer
 from .models import Event
-
+from apps.users.models import UserEvent
 
 
 class EventAPI(AuthenticatedJWT, ListAPIView):
     model = Event
     queryset = Event.objects.all().order_by('-event_date')
     serializer_class = EventSerializer
+
+
+class EventRegisterUserAPI(AuthenticatedJWT, APIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = EventRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        event_id = serializer.data.get('event_id')
+        UserEvent.objects.created(event_id=event_id, user=request.user)
+        return Response({'status': 'ok'}, status=status.HTTP_201_CREATED)
